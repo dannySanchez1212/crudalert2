a65d3ac8131b2da9b9f9dc4d708e240b19da3060
DESKTOP-C633K6V\FreddyAliendres <0000>
2018-04-09 17:48:09 -0400

    no message

--- CrudSweetAlert/app/Http/Controllers/ContactController.php
+++ CrudSweetAlert/app/Http/Controllers/ContactController.php
@@ -9,14 +9,23 @@ use App\Contact;
 
 class ContactController extends Controller
 {
-    
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
         //
         return view('welcome');
     }
 
-    
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function store(Request $request)
     {
         $data = [
